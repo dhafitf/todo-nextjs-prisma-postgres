2d185b163341eb62a237e16a0c12d95a3b7309c9
Dhafit Farenza <dhafidfarenza@gmail.com>
2023-08-10 01:23:23 +0700

    change post to todo

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -4,7 +4,7 @@ import prisma from "../../../lib/prisma";
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   const { title, content } = req.body;
 
-  const result = await prisma.post.create({
+  const result = await prisma.todo.create({
     data: {
       title: title,
       content: content,
